7c135065d58d0016de3d3f3f687bff21f1e974f6
Daniel B. Alves <71525341+danbalves@users.noreply.github.com>
2022-05-01 00:42:21 -0300

    wip

--- app/src/test/java/br/com/ioasys/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/br/com/ioasys/diceroller/ExampleUnitTest.kt
@@ -13,6 +13,6 @@ class ExampleUnitTest {
     fun generates_number(){
         val dice = Dice(6)
         val rollResult = dice.roll()
-        assertTrue("The value of rollResult was not between 1 and 6",rollResult in 1..6)
+        assertTrue(rollResult in 1..6)
     }
 }
